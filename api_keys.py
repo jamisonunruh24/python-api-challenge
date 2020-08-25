aec1fe288f1362d8897795a9450faaf0c229905f
Jamison Unruh <jamisonunruh24@gmail.com>
2020-08-24 19:13:47 -0500

    testing git ignore

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "add5c76f2f3b50c3d51b96f7b577bcb9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCir782FzDB5KfSD4bpxKM_yO-x5GbAJ9M"
